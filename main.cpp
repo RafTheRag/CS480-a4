a63da7e051f9ef59909c2bb207b6d0a2fa77dbd9
Myato <mattmdj77@gmail.com>
2023-11-15 00:36:12 -0800

    Comments

--- main.cpp
+++ main.cpp
@@ -18,40 +18,44 @@ using namespace std;
 
 int main(int argc, char** argv) {
     
-    int option = 0;
-    
+    //Creates ProducerConsumerMonitor class object (used for sharing data between main, producer and consumers)
     ProducerConsumerMonitor monitorData;
 
-
+    //Creates two class objects for the producers
     ProducerData BTCData;
     ProducerData ETHData;
 
+    //Gives specific types to each object (each share monitorData)
     BTCData.broker = &monitorData;
     BTCData.type = Bitcoin;
 
     ETHData.broker = &monitorData;
     ETHData.type = Ethereum;
 
-    ProducerData coinProducers[RequestTypeN] = {BTCData, ETHData};
+    ProducerData coinProducers[RequestTypeN] = {BTCData, ETHData};  //used for producer's optional arguements
 
+    //Creates two class objects for the producers
     ConsumerData blockChainXData;
     ConsumerData blockChainYData;
 
+    //Gives specific types to each object (each share monitorData)
     blockChainXData.broker = &monitorData;
     blockChainXData.type = BlockchainX;
 
     blockChainYData.broker = &monitorData;
     blockChainYData.type = BlockchainY;
 
-    ConsumerData coinConsumers[ConsumerTypeN] = {blockChainXData, blockChainYData};
-    
+    ConsumerData coinConsumers[ConsumerTypeN] = {blockChainXData, blockChainYData}; //used for consumer's optional arguements
 
+    int option = 0;
+    
+    //Contains the code for optional arguements for command line
     while ((option = getopt(argc, argv, "n:x:y:b:e:")) != -1) {
         switch (option) {
             
             case 'n':
-                monitorData.numOfTradeRequests = atoi(optarg);
-                //checks if numOfProgressMarks is greater than 10 to run
+
+                monitorData.numOfTradeRequests = atoi(optarg);                          //Total number of trade requests (production limit). The default is 120 if not specified
                 if (monitorData.numOfTradeRequests < 0){
                     cerr << "Number of trade requests must be atleast 1" << endl;
                     exit(EXIT_FAILURE);
@@ -59,19 +63,19 @@ int main(int argc, char** argv) {
                 break;
 
             case 'x':
-                coinConsumers[BlockchainX].timeToConsume = atoi(optarg) * 1000;                
+                coinConsumers[BlockchainX].timeToConsume = atoi(optarg) * 1000;        //Specifies the number of milliseconds N that the consumer using Blockchain X for processing a trade request and completing its transaction.   
                 break;
 
             case 'y':
-                coinConsumers[BlockchainY].timeToConsume = atoi(optarg) * 1000;
+                coinConsumers[BlockchainY].timeToConsume = atoi(optarg) * 1000;        //Specifies the number of milliseconds N that the consumer using Blockchain Y for processing a trade request and completing its transaction.
                 break;
 
             case 'b':
-                coinProducers[Bitcoin].timeToProduce = atoi(optarg) * 1000;
+                coinProducers[Bitcoin].timeToProduce = atoi(optarg) * 1000;            //Specifies the number of milliseconds required to produce and publish a Bitcoin request.
                 break;
 
             case 'e':
-                coinProducers[Ethereum].timeToProduce = atoi(optarg) * 1000;
+                coinProducers[Ethereum].timeToProduce = atoi(optarg) * 1000;           //Specifies the number of milliseconds required to produce and publish a Ethereum request.
                 break;
 
 
@@ -81,15 +85,22 @@ int main(int argc, char** argv) {
         }
     }
 
+
+
     sem_init(&monitorData.barrierSem, 0, 0);
 
+    //creates four threads(two for producers and two for comsumers)
     pthread_t producers[RequestTypeN];
     pthread_t consumers[ConsumerTypeN];
+
+    //initializes all the pthread mutexs and conditionals
     pthread_mutex_init(&monitorData.queueMutex, NULL);
     pthread_cond_init(&monitorData.notEmpty, NULL);
     pthread_cond_init(&monitorData.notFull, NULL);
     pthread_cond_init(&monitorData.BTCNotFull, NULL);
 
+
+    //starts the four threads(two for producers and two for comsumers), each one calls on either a producer or consumer thread function to either generate or consume for its specified request type or blockchain
     pthread_create(&producers[Bitcoin], NULL, &producer, (void*)&coinProducers[Bitcoin]);
     pthread_create(&producers[Ethereum], NULL, &producer, (void*)&coinProducers[Ethereum]);
     pthread_create(&consumers[BlockchainX], NULL, &consumer, (void*)&coinConsumers[BlockchainX]);
@@ -103,17 +114,18 @@ int main(int argc, char** argv) {
     pthread_join(consumers[BlockchainX], NULL);
     pthread_join(consumers[BlockchainY], NULL);
 
+
+    //our way of returning/updating the values of each consumer after the consumer threads run
     blockChainXData.blockChainConsumed[Bitcoin] = monitorData.BTCConsumedByX;
     blockChainXData.blockChainConsumed[Ethereum] = monitorData.ETHConsumedByX;
 
     blockChainYData.blockChainConsumed[Bitcoin] = monitorData.BTCConsumedByY;
     blockChainYData.blockChainConsumed[Ethereum] = monitorData.ETHConsumedByY;
 
-
     unsigned int* consumedSummary[ConsumerTypeN] = {blockChainXData.blockChainConsumed, blockChainYData.blockChainConsumed};
 
-    //std::cout << &blockChainXData.blockChainConsumed[Bitcoin] << " : " << &blockChainXData.blockChainConsumed[Ethereum] << std::endl;
 
+    //function uses the array for the producers and the 2d array for the consumers to print out a progress report after the thread are done running
     report_production_history(monitorData.produced, consumedSummary);
 
     exit(0);
--- monitor.h
+++ monitor.h
@@ -1,24 +1,29 @@
+/*
+ * Author: Rafael Ramirez & Matt D Jaranilla
+ * RedID: 825477088 & 825452453
+ */
 #ifndef MONITORS_H
 #define MONITORS_H
 
 #include <pthread.h>
 #include <semaphore.h>
 #include <queue>
-#include "tradecrypto.h"  // Include the necessary structures
+#include "tradecrypto.h"  
 class ProducerConsumerMonitor{
     public:
         sem_t barrierSem;
-        pthread_mutex_t queueMutex;          // Mutex for protecting critical sections
-        pthread_cond_t notEmpty;      // Condition variable for signaling unconsumed items
-        pthread_cond_t notFull;   // Condition variable for signaling available slots
-        pthread_cond_t BTCNotFull;
-        long unsigned int capacity = 15;
-        std::queue<RequestType> brokerQueue;
+        pthread_mutex_t queueMutex;           // Mutex for protecting critical sections
+        pthread_cond_t notEmpty;              // Condition variable for signaling unconsumed items
+        pthread_cond_t notFull;               // Condition variable for signaling available slots
+        pthread_cond_t BTCNotFull;            // Condition variable for signaling if there is 6 bitcoin in the queue
+        long unsigned int capacity = 15;      
+        std::queue<RequestType> brokerQueue;    //shared queue
 
-        unsigned int inRequestQueue[RequestTypeN] = {0, 0};
-        unsigned int produced[RequestTypeN] = {0, 0};
+        unsigned int inRequestQueue[RequestTypeN] = {0, 0}; //used to see what is currently in the queue
+        unsigned int produced[RequestTypeN] = {0, 0};       
         
-        unsigned int numOfTradeRequests = 120;
+        //intitalizing variables
+        unsigned int numOfTradeRequests = 120; 
         unsigned int coinsProduced = 0;
         unsigned int BTCCount = 0;
         unsigned int coinsConsumed = 0;
